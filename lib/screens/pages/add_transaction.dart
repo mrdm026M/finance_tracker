1c82088f4fdfd3e09a9c637b6ee1f686a1e8bdb2
Dhruv Maheshwari <mrdm026@gmail.com>
2022-02-09 01:54:08 +0530

    Created other pages of the page

---
+++ lib/screens/pages/add_transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTransaction extends StatefulWidget {
+  const AddTransaction({Key? key}) : super(key: key);
+
+  @override
+  State<AddTransaction> createState() => _AddTransactionState();
+}
+
+class _AddTransactionState extends State<AddTransaction> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("The Main Page")));
+  }
+}
---
+++ lib/screens/pages/graph_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GraphPage extends StatefulWidget {
+  const GraphPage({Key? key}) : super(key: key);
+
+  @override
+  State<GraphPage> createState() => _GraphPageState();
+}
+
+class _GraphPageState extends State<GraphPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("The Graph Page")));
+  }
+}
