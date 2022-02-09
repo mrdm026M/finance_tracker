0fb1f2dc1830309169aa26456137b1a43a9b558a
Dhruv Maheshwari <mrdm026@gmail.com>
2022-02-10 01:38:02 +0530

    Top part of UI complete

--- lib/screens/pages/main_page.dart
+++ lib/screens/pages/main_page.dart
@@ -1,7 +1,12 @@
+import 'package:finance_tracker/components/pages_components/main_page/my_total_balance.dart';
+import 'package:finance_tracker/components/pages_components/main_page/top_appbar.dart';
+import 'package:finance_tracker/components/pages_components/main_page/type_balance.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+  const MainPage({Key? key, required this.bankName}) : super(key: key);
+
+  final String bankName;
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -10,10 +15,46 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    var _width = MediaQuery.of(context).size.width;
+    var _height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text("The Main Page"),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+            width: _width,
+            height: _height,
+            color: Colors.grey.shade900,
+            child: SizedBox(
+              width: _width,
+              child: Column(
+                children: [
+                  topAppBar(widget.bankName),
+                  const SizedBox(height: 34.0),
+                  myTotalBalance(_width, "Rs. 20,000"),
+                  const SizedBox(height: 34.0),
+                  typeBalance(_width),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(24.0),
+            width: _width,
+            height: _height / 1.85,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
